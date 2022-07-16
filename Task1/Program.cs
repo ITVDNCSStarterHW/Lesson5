b7ca220727d6eee04b9ed7cc6d32b8b06cc0df44
Никита Бочаров <bochanikita@yandex.ru>
2022-07-16 13:19:12 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte operand1;
+            byte checkValue = 1;
+            byte result;
+
+            Console.WriteLine("Число для проверки");
+            operand1 = Convert.ToByte(Console.ReadLine());
+
+            result =(byte)(operand1 & checkValue);
+
+            if (result == 0)
+            {
+                Console.WriteLine("Число {0} четное", operand1);
+            }
+            else
+            {
+                Console.WriteLine("Число {0} нечетное", operand1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exp;
+            double money;
+
+            Console.WriteLine("Введите стаж сотрудника");
+            exp = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Зарплата сотрудника");
+            money = Convert.ToDouble(Console.ReadLine());
+
+
+            if (exp < 5)
+            {
+                money *= 1.1; 
+            }
+            else if (5 <= exp && exp < 10) 
+            {
+                money *= 1.15;
+            }
+            else if (10 <= exp && exp < 15)
+            {
+                money *= 1.25;
+            }
+            else if (15 <= exp && exp < 20)
+            {
+                money *= 1.35;
+            }
+            else if (20 <= exp && exp < 25)
+            {
+                money *= 1.4;
+            }
+            else if (exp >= 25)
+            {
+                money *= 1.5;
+            }
+            Console.WriteLine(money);
+            Console.ReadLine();
+        }
+    }
+}
